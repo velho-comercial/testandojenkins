4ae1b1555434337d639ade899a53ed40a655ea75
velho-comercial <velho.comercial@gmail.com>
2023-03-15 13:43:16 -0300

    primeiro commit

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Encontre Prestadores de Serviços' });
+});
+
+
+router.get('/resultado', function(req, res, next) {
+  res.render('resultado', { title: 'Resultado Busca',  trabalhadores : [ {nome: 'Diego Alves do Carmo'}, {nome: 'Thiago Alves do Carmo'}] });
+});
+
+module.exports = router;
